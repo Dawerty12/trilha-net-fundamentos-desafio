2986fa5cfff702787eb70719a66bb387057b342d
Dawerty12 <lnobre2003@gmail.com>
2022-11-10 01:53:11 -0300

    Resolvido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -5,6 +5,10 @@ namespace DesafioFundamentos.Models
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
+        public string placa;
+        public string modelo;
+        public string nome;
+        public string codigo;
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -14,14 +18,19 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-        
-            Console.WriteLine("Digite a placa do veículo, o nome do dono, o modelo do carro e código do cliente para estacionar:");
-            var placa = Console.ReadLine();
-            var nome = Console.ReadLine();
-            var modelo = Console.ReadLine();
-            var código = Console.ReadLine();
-            veiculos.Add(modelo + nome + código + placa);
-           
+          if (veiculos.Count() < 12){ 
+            Console.WriteLine("Digite a placa do veículo, o nome do dono, o modelo do carro e codigo do cliente para estacionar:");
+              placa = Console.ReadLine();
+              nome = Console.ReadLine();
+              modelo = Console.ReadLine();
+              codigo = Console.ReadLine();
+            veiculos.Add(modelo);
+            veiculos.Add(nome);
+            veiculos.Add(codigo);
+            veiculos.Add(placa);
+           }
+          else
+            Console.WriteLine("O estacionamento está lotado");
         }
 
         public void RemoverVeiculo()
@@ -29,10 +38,12 @@ namespace DesafioFundamentos.Models
             
 
             Console.WriteLine("Por favor, para remover o veículo, digite o id de resgate");
-            var código = Console.ReadLine();
+             codigo = Console.ReadLine();
+             
+            
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == código.ToUpper()))
+            if (veiculos.Any(x => x.ToUpper() == codigo.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
@@ -40,13 +51,18 @@ namespace DesafioFundamentos.Models
                 int horas = int.Parse(Console.ReadLine());
                 decimal valorTotal = (precoInicial + precoPorHora) * horas; 
                 
-                veiculos.Remove(código);
+                
+                veiculos.Remove(codigo);
+                veiculos.Remove(placa);
+                veiculos.Remove(nome);
+                veiculos.Remove(modelo);
 
-                Console.WriteLine($"O veículo de id {código} foi removido e o preço total foi de: R$ {valorTotal}");
+
+                Console.WriteLine($"O veículo de id {codigo} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou o código corretamente");
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou o codigo corretamente");
             }
         }
 
@@ -55,14 +71,29 @@ namespace DesafioFundamentos.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+               
+                
                 Console.WriteLine("Os veículos estacionados são:");
-              
-                foreach (string veiculo in veiculos)
-                {
-                    
-                    Console.WriteLine(veiculo);
-                    
-                }
+                Console.WriteLine(veiculos[0]);
+                Console.WriteLine(veiculos[1]);
+                Console.WriteLine(veiculos[2]);
+                Console.WriteLine(veiculos[3]);
+                Console.WriteLine("");
+                Console.WriteLine(veiculos[4]);
+                Console.WriteLine(veiculos[5]);
+                Console.WriteLine(veiculos[6]);
+                Console.WriteLine(veiculos[7]);
+                Console.WriteLine("");
+                Console.WriteLine(veiculos[8]);
+                Console.WriteLine(veiculos[9]);
+                Console.WriteLine(veiculos[10]);
+                Console.WriteLine(veiculos[11]);
+
+
+                
+                         
+
+                
             }
             else
             {
